a925d9fe4c66455006bd9032d0b3d19316fea007
Elvia Attack <elviaatt@mi-m6.msk.21-school.ru>
2023-09-23 13:33:49 +0300

    develop/moved backend logic in model folder. Tests work

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,14 @@ int main(int argc, char *argv[]) {
   w.show();
   return a.exec();
 }
+
+// #include "consoleView.h"
+
+// int main() {
+//     ExampleModel model;
+//     ExampleController controller(&model);
+//     ConsoleView view(&controller);
+//     view.startEventLoop();
+//     return 1;
+// }
+
--- src/tests/test.c
+++ src/tests/test.c
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 
 
-#include "../SmartCalc.h"
+#include "../model/SmartCalc.h"
 
 #define EPS 1e-7
 #define EPS2 1e-2
