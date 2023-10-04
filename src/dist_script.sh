c0460e36eb269348cff675343222b3c9c1371af9
Elvia Attack <elviaatt@ga-i4.msk.21-school.ru>
2023-10-04 23:10:52 +0300

    develop/dist works

--- src/dist_script.sh
+++ src/dist_script.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
 
-mkdir SmartCalc_dist
+mkdir -p SmartCalc_dist SmartCalc_dist/model SmartCalc_dist/view SmartCalc_dist/controller SmartCalc_dist/tests
+
 cd SmartCalc_dist
-cp *.cpp/
-# cp ../*.c .
-# cp ../*.h .
-# cp ../*.cpp .
-# cp ../*.ui .
-# cp ../CMakeLists.txt .
-# cp ../CMakeLists.txt.user .
-# cp ../*.sh .
-# cp ../readme.html .
-# cp ../Makefile .
+cp ../*.cc . 2>/dev/null
+cp ../*.h . 2>/dev/null
+cp ../*.sh . 2>/dev/null
+cp ../readme.html . 2>/dev/null
+cp ../Makefile . 2>/dev/null
+cp ../CMakeLists.txt . 2>/dev/null
+cp ../../.gitignore . 2>/dev/null
+cp ../../CHANGELOG . 2>/dev/null
+cp ../../LICENSE . 2>/dev/null
+cp ../../README.md . 2>/dev/null
+cp ../../README_RUS.md . 2>/dev/null
+
+cp ../model/*.h model 2>/dev/null
+cp ../model/*.cc model 2>/dev/null
+cp ../model/CMakeLists.txt model 2>/dev/null
+
+cp ../controller/*.h controller 2>/dev/null
+cp ../controller/*.cc controller 2>/dev/null
+cp ../controller/CMakeLists.txt controller 2>/dev/null
+
+cp ../view/*.h view 2>/dev/null
+cp ../view/*.cc view 2>/dev/null
+cp ../view/*.ui view 2>/dev/null
+cp ../view/CMakeLists.txt view 2>/dev/null
+
+cp ../tests/*.h tests 2>/dev/null
+cp ../tests/*.cc tests 2>/dev/null
+
 cd ..
 
-tar -czf SmartCalc_arch.tar.gz SmartCalc_dist/
+tar -czf SmartCalc_mac.tar.gz SmartCalc_dist/
